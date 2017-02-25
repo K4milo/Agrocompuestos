b9285b8479da8df08c930b6ca52f105c919f7c1d
Camilo Andrés <kmilo.vanegas@gmail.com>
2017-02-25 02:24:46 -0500

    esquema inicial de tema

---
+++ wp-theme/index.php
@@ -0,0 +1,11 @@
+<?php get_template_part('includes/header'); ?>
+
+<div class="container">
+  <div class="row">
+    
+  
+    
+  </div><!-- /.row -->
+</div><!-- /.container -->
+
+<?php get_template_part('includes/footer'); ?>
